683e58d44b6b0db5d050107adfcabdeaf4ebccb1
victorien devaux <saiwaiohayou@gmail.com>
2018-12-21 16:56:10 +0100

    Base pour la base de donnée

--- app/src/main/java/viniki/ccm/insset/viniki/ActivitePrincipale.java
+++ app/src/main/java/viniki/ccm/insset/viniki/ActivitePrincipale.java
@@ -1,13 +1,56 @@
 package viniki.ccm.insset.viniki;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivitePrincipale extends AppCompatActivity {
+
+    private TextView loginTextView, passwordTextView;
+    private Button btn_connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activite_principale);
+        /*loginTextView = findViewById(R.id.txt_login);
+        passwordTextView = findViewById(R.id.txt_password);
+
+        Log.i("victorien", "login :" + loginTextView + "mot de passe :" + passwordTextView);*/
+
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        firebaseFirestore.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+
     }
 }
