c69cd846f1a637a1827b6c4054e9525322c7b4ae
Kilian <kilian.blamoutier@gmail.com>
2019-01-22 21:29:06 +0100

    Creation classe Localisation + fonction de distance

---
+++ app/src/main/java/viniki/ccm/insset/viniki/Localisation.java
@@ -0,0 +1,39 @@
+package viniki.ccm.insset.viniki;
+
+public class Localisation {
+
+    private double longitude;
+
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    private double deg2rad(double d){
+        return d * (Math.PI/180);
+    }
+
+    //Distance en m.
+    public double getDistanceWithOtherLocalisation(Localisation loc){
+        int R = 6371;
+        double dLat = deg2rad(loc.getLatitude()-this.getLatitude());
+        double dLon = deg2rad(loc.getLongitude()-this.getLongitude());
+        double result = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(this.getLatitude())) * Math.cos(deg2rad(loc.getLatitude())) * Math.sin(dLon/2) * Math.sin(dLon/2);
+        result = 2 * Math.atan2(Math.sqrt(result), Math.sqrt(1-result));
+        result = R * result;
+        return result*1000;
+    }
+}
