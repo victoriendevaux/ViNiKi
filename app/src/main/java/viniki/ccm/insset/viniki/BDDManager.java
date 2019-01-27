97671cdd73fab671319f55da4308db518c355d50
victoriendevaux <victorien.devaux@gmail.com>
2019-01-27 16:13:29 +0100

    annulation du commit

---
+++ app/src/main/java/viniki/ccm/insset/viniki/BDDManager.java
@@ -0,0 +1,209 @@
+package viniki.ccm.insset.viniki;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BDDManager {
+
+    private static FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    // Partie Utilisateur
+
+    private static String NomTableUtilisateur = "Utilisateur";
+
+    public static void addUtilisateur(Utilisateur newUtilisateur, String password, final Inscription context){
+
+        Map<String, Object> nouveauUtilisateur = new HashMap<>();
+
+        nouveauUtilisateur.put("nomUtilisateur", newUtilisateur.getNomUtilisateur());
+        nouveauUtilisateur.put("prenomUtilisateur", newUtilisateur.getPrenomUtilisateur());
+        nouveauUtilisateur.put("emailUtilisateur", newUtilisateur.getEmailUtilisateur());
+        nouveauUtilisateur.put("adresseUtilisateur", newUtilisateur.getAdresseUtilisateur());
+        nouveauUtilisateur.put("dateNaissanceUtilisateur", newUtilisateur.getDateNaissanceUtilisateur());
+        nouveauUtilisateur.put("status", "online");
+        nouveauUtilisateur.put("password", password);
+        // Options
+        nouveauUtilisateur.put("frequenceDeplacement", 5);
+        nouveauUtilisateur.put("porteeVisuel", 100);
+
+        firebaseFirestore
+                //nom de la base
+                .collection(NomTableUtilisateur)
+                .add(nouveauUtilisateur) //Ajout de la map
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Log.i("Ajoute_Map_Activite", "Add OK");
+                        Log.i("Ajoute_Map_Activite", task.getResult().getId());
+                        context.inscriptionSuccess(task.getResult().getId());
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Ajoute_Map_Activite", "Add Echec");
+                        context.inscriptionEchec();
+                    }
+                });
+    }
+
+    public static void loginUtilisateur(String login, String mdp, final Login context){
+        Task<QuerySnapshot> taskQuery = firebaseFirestore
+                .collection(NomTableUtilisateur)
+                .whereEqualTo("emailUtilisateur", login)
+                .whereEqualTo("password", mdp)
+                .get(Source.DEFAULT).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        Utilisateur utilisateur = new Utilisateur();
+                        if(task.getResult().size() != 1){
+                            // Pas la bonne récupération.
+                            context.loginEchec();
+                        }else{
+                            // Utilisateur trouvé.
+                            DocumentSnapshot result = task.getResult().getDocuments().get(0);
+                            utilisateur.setIdUtilisateur(result.getId());
+                            utilisateur.setNomUtilisateur((String) result.get("nomUtilisateur"));
+                            utilisateur.setPrenomUtilisateur((String) result.get("prenomUtilisateur"));
+                            utilisateur.setEmailUtilisateur((String) result.get("nomUtilisateur"));
+                            utilisateur.setAdresseUtilisateur((String) result.get("nomUtilisateur"));
+                            utilisateur.setDateNaissanceUtilisateur((Date) result.get("dateNaissanceUtilisateur"));
+                            // Options
+                            utilisateur.setFrequenceDeplacement((Long) result.get("frequenceDeplacement"));
+                            utilisateur.setPorteeVisuel((Long) result.get("porteeVisuel"));
+
+                            utilisateur.setMaLocalisation(new Localisation());
+
+                            GlobalVariable.getInstance().setConnectedUtilisateur(utilisateur);
+                            changeStatusUtilisateur(result.getId(), true);
+
+                            context.loginSuccess();
+                        }
+
+                    }
+                });
+    }
+
+    public static void emailExiste(String email, final Inscription context){
+        Task<QuerySnapshot> taskQuery = firebaseFirestore
+                .collection(NomTableUtilisateur)
+                .whereEqualTo("emailUtilisateur", email)
+                .get(Source.DEFAULT).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        Utilisateur utilisateur = new Utilisateur();
+                        if(task.getResult().size() != 0){
+                            // Existant
+                            context.emailUncheck();
+                        }else{
+                            // Nouveau
+                            context.emailCheck();
+                        }
+                    }
+                });
+    }
+
+    public static void changeStatusUtilisateur(String idUtilisateur, boolean estConnect){
+        Map<String, Object> statusChange = new HashMap<>();
+        if (estConnect){
+            // Connexion
+            statusChange.put("status", "online");
+        }else{
+            // Deconnexion
+            statusChange.put("status", "offline");
+        }
+        firebaseFirestore.collection(NomTableUtilisateur).document(idUtilisateur).update(statusChange);
+    }
+
+    public static void logoutUtilisateur(){
+        changeStatusUtilisateur(GlobalVariable.getInstance().getConnectedUtilisateur().getIdUtilisateur(), false);
+    }
+
+    public static void getUtilisateursOnline(final MapsActivity context){
+        Task<QuerySnapshot> taskQuery = firebaseFirestore
+                .collection(NomTableUtilisateur)
+                .whereEqualTo("status", "online")
+                .get(Source.DEFAULT).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        List<String> tabIdUtilisateur = new ArrayList<String>();
+                        for(int i = 0; i < task.getResult().size(); i++){
+
+                            DocumentSnapshot result = task.getResult().getDocuments().get(i);
+
+                            if(result.getId() != GlobalVariable.getInstance().getConnectedUtilisateur().getIdUtilisateur()){
+                                // Pas l'utilisateur en cours
+                                tabIdUtilisateur.add(result.getId());
+                            }
+
+                        }
+
+                        getLocalisationsUtilisateurs(tabIdUtilisateur, context);
+                    }
+                });
+    }
+
+    // Partie Localisation
+
+    private static String NomTableLocalisation = "Localisation";
+
+
+    public static void getLocalisationsUtilisateurs(final List<String> tabIdUtilisateur, final MapsActivity context){
+            firebaseFirestore
+                    .collection(NomTableLocalisation)
+                    .get(Source.DEFAULT)
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                         @Override
+                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                             List<Localisation> tabLocalisation = new ArrayList<Localisation>();
+
+                             for (QueryDocumentSnapshot unResult : task.getResult()){
+                                 if(!unResult.getId().equals(GlobalVariable.getInstance().getConnectedUtilisateur().getIdUtilisateur()) && tabIdUtilisateur.contains(unResult.getId())){
+                                     //
+                                     Log.i("LPK_GetLoK", "Vrai ! " + unResult.getId());
+                                     Log.i("LPK_GetLoK", "LONG ! " + unResult.get("longitude"));
+                                     Log.i("LPK_GetLoK", "LATI ! " + unResult.get("latitude"));
+                                     Localisation laLoc = new Localisation();
+                                     laLoc.setLatitude((double) unResult.get("latitude"));
+                                     laLoc.setLongitude((double) unResult.get("longitude"));
+                                     tabLocalisation.add(laLoc);
+                                 }
+                                 else{
+                                     Log.i("LPK_GetLoK", "Faux ! " + unResult.getId());
+                                 }
+                             }
+
+                             context.rafraichirPositionUtilisateurs(tabLocalisation);
+                         }
+                    });
+    }
+
+    public static void changeLocalisationUtilisateur(){
+        Map<String, Object> newLocalisation = new HashMap<>();
+
+        newLocalisation.put("longitude", GlobalVariable.getInstance().getConnectedUtilisateur().getMaLocalisation().getLongitude());
+        newLocalisation.put("latitude", GlobalVariable.getInstance().getConnectedUtilisateur().getMaLocalisation().getLatitude());
+
+        firebaseFirestore
+                .collection(NomTableLocalisation)
+                .document(GlobalVariable.getInstance().getConnectedUtilisateur().getIdUtilisateur())
+                .set(newLocalisation);
+    }
+}
---
+++ app/src/main/java/viniki/ccm/insset/viniki/GPSLocalisationService.java
@@ -0,0 +1,94 @@
+package viniki.ccm.insset.viniki;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+public class GPSLocalisationService extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+    private static MapsActivity activiteMap = null;
+
+    public GPSLocalisationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i("LPK_LOK_Listener", "onCreate");
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Log.i("LPK_LOK_Listener", "" + location.getLongitude() + " / " + location.getLatitude());
+                GlobalVariable.getInstance().getConnectedUtilisateur().getMaLocalisation().setLongitude(location.getLongitude());
+                GlobalVariable.getInstance().getConnectedUtilisateur().getMaLocalisation().setLatitude(location.getLatitude());
+
+                if(activiteMap != null){
+                    // L'utilisateur se trouve sur la map.
+                    BDDManager.getUtilisateursOnline(activiteMap);
+                }
+
+                BDDManager.changeLocalisationUtilisateur();
+                //Log.i("LPK_LOK_Listener", "" + location.getLongitude() + " / " + location.getLatitude());
+
+                // Mettre à jour le temps de raffraichissement...
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent monIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                monIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(monIntent);
+            }
+        };
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        // Actualisation des données GPS faite toutes les 0.1 secondes si un changement est survenue.
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Si pas les permissions
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, BoiteAOutils.getTempsRafraichissement(this), 0, listener);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null){
+            locationManager.removeUpdates(listener);
+        }
+    }
+
+    public static void setActiviteMap(MapsActivity activiteMap) {
+        GPSLocalisationService.activiteMap = activiteMap;
+    }
+
+}
